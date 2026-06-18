e1870571078d66077c177cd3c0b94aaa1e36a2da
JoeNik <154469502@qq.com>
2026-06-19 00:21:48 +0800

    Bump version to 2.7.12+114

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -11,7 +11,7 @@ import 'tts_settings_page.dart';
 
 import 'package:package_info_plus/package_info_plus.dart';
 
-const String appVersion = '2.7.11';
+const String appVersion = '2.7.12';
 
 /// 应用设置页面
 class SettingsPage extends StatelessWidget {
